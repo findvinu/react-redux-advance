e5f6900eff3a25ae65ee0b59a22effbeb474d0dc
findvinu <vinodyadavhhh4@gmail.com>
2024-01-14 16:08:52 +0530

    notification

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch(
-      "https://react-redux-toolkit-82fc1-default-rtdb.firebaseio.com/cart.json",
-      { method: "PUT", body: JSON.stringify(cart) }
-    );
+    const sendCardData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending",
+          message: "Sending card data",
+        })
+      );
+
+      const response = await fetch(
+        "https://react-redux-toolkit-82fc1-default-rtdb.firebaseio.com/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending card data failed");
+      }
+
+      const responseData = await response.json();
+    };
+
+    sendCardData();
   }, [cart]);
   return (
     <Layout>
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUISliceState = { cartIsVisible: false };
+const initialUISliceState = { cartIsVisible: false, notification: null };
 
 const uiSlice = createSlice({
   name: "ui",
@@ -8,6 +8,13 @@ const uiSlice = createSlice({
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible; // redux-toolkit using third party immer library which translated this mutable code into inmutable inernaly.
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: state.notification.status,
+        title: state.notification.title,
+        message: state.notification.message,
+      };
     },
   },
 });
